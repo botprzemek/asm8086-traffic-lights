d4ca0d4f2dbec52beebe91c4cfd8c4e6a961ee6b
botprzemek <info@botprzemek.pl>
2024-11-12 10:38:31 +0100

    fix(repo): remade project structure and added LICENSE file and section to README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Traffic Lights simulator - Assembly 8086
+
+This project was made for college exam on the Computer Architecture lectures, written purely in Intel's 8086 Assembly.
+
+## License
+
+This project is licensed under the Apache License, Version 2.0. Key points are: permissions and conditions of usage. For
+more detailed information, please refer to the [LICENSE file](./LICENSE)
+
+## Contributors
+
+[![An image of @botprzemek's project contributors](https://contrib.rocks/image?repo=botprzemek/asm8086-traffic-lights)](https://github.com/botprzemek)
--- src/main.asm
+++ src/main.asm
@@ -9,7 +9,7 @@
     green equ 00h
     amber equ 10h
     red equ 01h
-    red_amber equ 11h    
+    red_amber equ 11h
 
 .code
 start:
@@ -19,25 +19,25 @@ start:
 
 wait:
     ; Check the state of the detector on the side road
-    in al, detector   ; 00h or 01h depending on whether there is a vehicle or not
-    test al, 01h      ; AND operation between register AL and 01h
-    jz wait           ; If no vehicle, continue waiting
+    in al, detector ; 00h or 01h depending on whether there is a vehicle or not
+    test al, 01h    ; AND operation between register AL and 01h
+    jz wait         ; If no vehicle, continue waiting
 
     ; Light change sequence
-    
+
     ; Amber on the main road, 3 seconds delay
-    mov al, amber              ; 10h -> AL
-    out portM, al              ; AL -> 0C1h
+    mov al, amber ; 10h -> AL
+    out portM, al ; AL -> 0C1h
     call delay_3_seconds
-    
+
     ; Red on the main road, 3 seconds delay
-    mov al, red                ; 01h -> AL
-    out portM, al              ; AL -> 0C1h
+    mov al, red   ; 01h -> AL
+    out portM, al ; AL -> 0C1h
     call delay_3_seconds
 
     ; Red with amber on the side road, 3 seconds delay
-    mov al, red_amber          ; 11h -> AL
-    out portS, al              ; AL -> 06Bh
+    mov al, red_amber ; 11h -> AL
+    out portS, al     ; AL -> 06Bh
     call delay_3_seconds
 
     ; Green on the side road, 13 seconds delay
@@ -46,12 +46,12 @@ wait:
     call delay_13_seconds
 
     ; Return to initial state
-    
+
     ; Amber on the side road
     mov al, amber
     out portS, al
     call delay_3_seconds
-    
+
     ; Red on the side road
     mov al, red
     out portS, al
@@ -66,13 +66,13 @@ wait:
     mov al, green
     out portM, al
     call delay_13_seconds
-    jmp wait  ; Return to checking the detector
+    jmp wait ; Return to checking the detector
 
 ; Procedure for 3 seconds delay
 delay_3_seconds proc
     push cx
     push dx
-    mov cx, 3        ; execute 3 times (3 seconds)
+    mov cx, 3 ; Execute 3 times (3 seconds)
     call delay_1_second
     pop dx
     pop cx
@@ -83,7 +83,7 @@ delay_3_seconds endp
 delay_13_seconds proc
     push cx
     push dx
-    mov cx, 13       ; execute 13 times (13 seconds)
+    mov cx, 13 ; Execute 13 times (13 seconds)
     call delay_1_second
     pop dx
     pop cx
@@ -93,7 +93,7 @@ delay_13_seconds endp
 ; Procedure for 1 second delay
 delay_1_second proc
     pusha
-    mov cx, 18              ; Approximately 1 second at standard clock frequency
+    mov cx, 18 ; Approximately 1 second at standard clock frequency
 delay_loop:
     mov ah, 2Ch
     int 21h
@@ -103,7 +103,7 @@ loop2:
     int 21h
     cmp dh, bl
     je loop2
-    loop delay_loop   
+    loop delay_loop
     popa
     ret
 delay_1_second endp
